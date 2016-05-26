be44a474840d5ae7e1f2bb6d10ef8ff9d618e102
Les Orchard <me@lmorchard.com>
2016-05-26 15:16:39 -0600

    Add missing mozilla_cloud_services_logger.django package, bump to v1.0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'mozilla-cloud-services-logger',
-  packages = ['mozilla_cloud_services_logger'],
-  version = '1.0',
+  packages = ['mozilla_cloud_services_logger', 'mozilla_cloud_services_logger.django']
+  version = '1.0.1',
   description = 'Tools for producing a common application logging format defined by Mozilla Cloud Services',
   author = 'Les Orchard',
   author_email = 'lorchard@mozilla.com',
   url = 'https://github.com/mozilla/mozilla-cloud-services-logger',
-  download_url = 'https://github.com/mozilla/mozilla-cloud-services-logger/tarball/1.0',
+  download_url = 'https://github.com/mozilla/mozilla-cloud-services-logger/tarball/1.0.1',
   keywords = ['mozilla', 'logging', 'django'],
   classifiers = [
      'Development Status :: 4 - Beta',
